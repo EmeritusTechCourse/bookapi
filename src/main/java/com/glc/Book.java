699ee8b2c6f5499b73fc4a0603cb38b175fb0861
Hunaid Baig <hunaid.baig@xloopdigital.com>
2023-04-07 02:44:51 +0500

    I have successFully passed the second test addBook()

--- src/main/java/com/glc/Book.java
+++ src/main/java/com/glc/Book.java
@@ -1,11 +1,40 @@
 package com.glc;
+import java.util.ArrayList;
 
 public class Book {
     
-    public int bookList = 0;
+    public String title;
+    public String author;
+    public int length;
+    public int year;
+    public ArrayList<String> bookList = new ArrayList<String>();
+
+    Book(){
+    }
+
+    Book(String title, String author, int length, int year){
+        this.title = title;
+        this.author = author;
+        this.length = length;
+        this.year = year;
+    }
+
+    public void addBook(Book obj, String dateRead, int rating){
+        //int length = bookList.length;
+
+        String ratingStar = "";
+        for (int i = 0; i < rating-1; i++) {
+            ratingStar += "*"; 
+        }
+        bookList.add(obj.title + "by" + obj.author +"," + obj.length + "pages," + "read on" + dateRead + " "+ ratingStar) ;
+
+    }
+
+
+
 
     public int numberRead(){
-        return bookList;
+        return bookList.size();
     }
 
 }
