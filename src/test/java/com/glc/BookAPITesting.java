cb371bfacb700749c65f5d499044f010d128acd5
Hunaid Baig <hunaid.baig@xloopdigital.com>
2023-04-07 02:43:23 +0500

    My Second test case has failed addBook()

--- src/test/java/com/glc/BookAPITesting.java
+++ src/test/java/com/glc/BookAPITesting.java
@@ -12,15 +12,25 @@ public class BookAPITesting {
     public void emptyListTest(){
         //setup
         int list = 0;
-
         Book bookObject = new Book();
-
         //execute
         int result = bookObject.numberRead();
-
         //Assert
         assertEquals(list, result);
     }
 
+    //Given that I have an empty list, when I add the first book to my list then I expect numberRead to return 1.
+    @Test
+    public void addFirstBookTest(){
+        //setup
+        int list = 1;
+        Book bookObject = new Book("The Hobbit", "J.R.R. Tolkein", 320, 1937);
+        bookObject.addBook(bookObject, "January 1, 2020", 5);
+
+        //execute
+        int result = bookObject.numberRead();
+        //Assert
+        assertEquals(list, result);
+    }
 
 }
