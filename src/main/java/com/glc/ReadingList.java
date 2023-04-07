d8c70041172d349f5274d7878d68e438092fd57c
hunaidbaig <hunaid.baig@xloopdigital.com>
2023-04-07 10:08:08 +0500

    My Fourth test case failed removeBookTest()

---
+++ src/main/java/com/glc/ReadingList.java
@@ -0,0 +1,36 @@
+package com.glc;
+
+import java.util.ArrayList;
+
+public class ReadingList {
+
+    public ArrayList<String> bookList = new ArrayList<String>();
+
+    public void addBook(Book obj, String dateRead, int rating){
+        //int length = bookList.length;
+
+        String ratingStar = "";
+        for (int i = 0; i < rating-1; i++) {
+            ratingStar += "*"; 
+        }
+        bookList.add(obj.title + "by" + obj.author +"," + obj.length + "pages," + "read on" + dateRead + " "+ ratingStar) ;
+
+    }
+
+    public void removeBook(String title){        
+        for (int index = 0; index < bookList.size(); index++) {
+            String firstElement = bookList.get(index);
+
+            if(firstElement.contains(title)){
+                bookList.remove(index);
+                break;
+            }
+        }
+    }
+
+
+    public int numberRead(){
+        return bookList.size();
+    }
+
+}
--- src/test/java/com/glc/BookAPITesting.java
+++ src/test/java/com/glc/BookAPITesting.java
@@ -57,7 +57,36 @@ public class BookAPITesting {
         assertEquals(list, result);
     }
 
-    
+    //Given that I have a book in my list, when I call removeBook("<title>") with "title" representing the title of my book that I want to delete, then when I call getBooks() the book I deleted should no longer be there.
+    @Test
+    public void removeBookTest(){
+        //setup
+        String title = "The History";
+        
+
+        Book bookObject1 = new Book("The Hobbit", "J.R.R. Tolkein", 320, 1937);
+        Book bookObject2 = new Book("The History", "Ozaib", 320, 1937);
+        Book bookObject3 = new Book("The Math", "Hunaid. Tolkein", 320, 1937);
+        Book bookObject4 = new Book("The Science", "Eienstien. Tolkein", 320, 1937);
+        
+        ReadingList readingObject = new ReadingList();
+
+        readingObject.addBook(bookObject1, "January 1, 2020", 5);
+        readingObject.addBook(bookObject2, "January 2, 2020", 4);
+        readingObject.addBook(bookObject3, "January 3, 2020", 4);
+        readingObject.addBook(bookObject4, "January 4, 2020", 3);
+
+        int length = readingObject.numberRead() - 1;
+
+        //execute
+        readingObject.removeBook(title);
+
+        //String[] returnBooks = readingObject.getBooks();
+        
+        int result = readingObject.numberRead();
+        //Assert
+        assertEquals(length, result);
+    }
 
 
 
