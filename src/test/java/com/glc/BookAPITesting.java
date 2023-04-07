b919e4318b8268dba316849ca71cf915d837585a
hunaidbaig <hunaid.baig@xloopdigital.com>
2023-04-07 09:41:53 +0500

    I have successFully passed my third test addAdditionalBookTest()

--- src/test/java/com/glc/BookAPITesting.java
+++ src/test/java/com/glc/BookAPITesting.java
@@ -12,9 +12,9 @@ public class BookAPITesting {
     public void emptyListTest(){
         //setup
         int list = 0;
-        Book bookObject = new Book();
+        ReadingList readingObject = new ReadingList();
         //execute
-        int result = bookObject.numberRead();
+        int result = readingObject.numberRead();
         //Assert
         assertEquals(list, result);
     }
@@ -25,12 +25,40 @@ public class BookAPITesting {
         //setup
         int list = 1;
         Book bookObject = new Book("The Hobbit", "J.R.R. Tolkein", 320, 1937);
-        bookObject.addBook(bookObject, "January 1, 2020", 5);
+        ReadingList readingObject = new ReadingList();
+
+        readingObject.addBook(bookObject, "January 1, 2020", 5);
 
         //execute
-        int result = bookObject.numberRead();
+        int result = readingObject.numberRead();
         //Assert
         assertEquals(list, result);
     }
+
+    //Given that I have an empty list, when I add additional books to the list I expect the numberRead to return the total number of books in my list.
+    @Test
+    public void addAdditionalBookTest(){
+        //setup
+        int list = 4;
+        Book bookObject1 = new Book("The Hobbit", "J.R.R. Tolkein", 320, 1937);
+        Book bookObject2 = new Book("The History", "Ozaib", 320, 1937);
+        Book bookObject3 = new Book("The Math", "Hunaid. Tolkein", 320, 1937);
+        Book bookObject4 = new Book("The Science", "Eienstien. Tolkein", 320, 1937);
+        ReadingList readingObject = new ReadingList();
+
+        readingObject.addBook(bookObject1, "January 1, 2020", 5);
+        readingObject.addBook(bookObject2, "January 2, 2020", 4);
+        readingObject.addBook(bookObject3, "January 3, 2020", 4);
+        readingObject.addBook(bookObject4, "January 4, 2020", 3);
+
+        //execute
+        int result = readingObject.numberRead();
+        //Assert
+        assertEquals(list, result);
+    }
+
+    
+
+
 
 }
